20ab2fe5777441f069997c58830c44bc37099762
Armando687 <jlopezrodriguez687@gmail.com>
2019-07-02 00:58:44 -0400

    implementar configuraciones de base de datos en arduino fix #6

--- arduino-sistemacontra-incendios/arduino-sistemacontra-incendios.ino
+++ arduino-sistemacontra-incendios/arduino-sistemacontra-incendios.ino
@@ -32,6 +32,12 @@ int historiaLed = 0;
 //variable de control
 int control = 0;
 
+//variables de congiguracion;
+int temperatura_min;
+int temperatura_max;
+int temperatura_media;
+int temperatura_critica;
+
 //intancia de objetos
 LiquidCrystal_I2C lcd(0x3f,16,2); 
 DHT dht1(4,DHT11);
@@ -40,7 +46,7 @@ DHT dht2(3,DHT11);
 //datos para conexion a servidor
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-IPAddress server_mysql(192,168,1,14);// IP of the MySQL *server* here
+IPAddress server_mysql(192,168,1,10);// IP of the MySQL *server* here
 IPAddress server_addr(192,168,1,50);// IP of the arduiono *server* here
 char user[] = "arduino";              // MySQL user login username
 char password[] = "arduino";        // MySQL user login password
@@ -50,6 +56,7 @@ char INSERT_DHT[] = "INSERT INTO db_arduino.temperatura_humedad(temperatura,hume
 char INSERT_LED[] = "INSERT INTO db_arduino.conexion_electrica(estado) VALUES (%d)";
 char INSERT_MT[] = "INSERT INTO db_arduino.bomba_hidraulica(estado) VALUES (%d)";
 char INSERT_VT[] = "INSERT INTO db_arduino.ventilador(estado) VALUES (%d)";
+char SELECT_ALL[] = "SELECT * FROM db_arduino.configuracion";
 char query[128];
 
 EthernetClient client;
@@ -62,7 +69,32 @@ void setup() {
   Ethernet.begin(mac_addr,server_addr);
   Serial.println("Connecting...");
   if (conn.connect(server_mysql,3306, user,password)) {
-    delay(1000);
+    delay(500);
+    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+    sprintf(query, SELECT_ALL);
+    cur_mem->execute(query);
+    column_names *columns = cur_mem->get_columns();
+    row_values *row = NULL;
+   do {
+      row = cur_mem->get_next_row();
+      if (row != NULL) {
+        temperatura_min = atol(row->values[1]);
+        temperatura_max = atol(row->values[2]);
+        temperatura_media = atol(row->values[3]);
+        temperatura_critica = atol(row->values[4]);
+      }
+    } while (row != NULL);
+     delete cur_mem;
+
+  // Show the result
+  Serial.print("  temperatura_min = ");
+  Serial.println(temperatura_min);
+  Serial.print("  temperatura_max = ");
+  Serial.println(temperatura_max);
+  Serial.print("  temperatura_media = ");
+  Serial.println(temperatura_media);
+  Serial.print("  temperatura_critica = ");
+  Serial.println(temperatura_critica);
   }
   else{
     Serial.println("Connection failed.");}
@@ -124,7 +156,7 @@ void loop() {
     historiaHumedad = humedadpromedio;
     
   }
-  if(temperaturapromedio >= 20 && temperaturapromedio <=27){
+  if(temperaturapromedio >= temperatura_min && temperaturapromedio <= temperatura_max){
     //solo hace calor
     statusVentilador = 1;
     digitalWrite(ventiladorPIN,HIGH); //encendemos el ventilador
@@ -135,14 +167,14 @@ void loop() {
     delay(2000);
     
   }
-  if(temperaturapromedio <= 19 && statusVentilador == 1){
+  if(temperaturapromedio <= temperatura_media && statusVentilador == 1){
     digitalWrite(ventiladorPIN,LOW);// apagamos ventilador
     delay(1000);
     statusVentilador = 0;
     historiaVentilador = statusVentilador;      
     saveEstadoVentilador();
   }
-  if(temperaturapromedio >= 28){
+  if(temperaturapromedio >= temperatura_critica){
     if(statusVentilador = 1){
       digitalWrite(ventiladorPIN,LOW);// apagamos ventilador
       delay(1000);
@@ -167,7 +199,7 @@ void loop() {
     
   }
   
-  if(temperaturapromedio <=27 && statusMotor == 1){
+  if(temperaturapromedio <=temperatura_max - 1 && statusMotor == 1){
     digitalWrite(bombaPIN,LOW);//apagamos la bomba de agua;
     statusMotor = 0;
     historiaMotor = statusMotor;
